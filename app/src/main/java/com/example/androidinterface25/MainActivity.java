18938e3f56da4ba3e454dad19cd8448ed3182268
Alexey Kapanov <alesnim@gmail.com>
2021-11-15 09:48:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidinterface25/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidinterface25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    // реализуйте методы изменения фонового цвета
+
+
+
+    //реалиуйте подсчет тортов через строку множественного числа
+}
